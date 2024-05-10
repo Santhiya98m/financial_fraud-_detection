8047a6958c5486b60680b87e70d96770376f285e
Santhiya98m <132222614+Santhiya98m@users.noreply.github.com>
2024-05-10 11:32:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-
-
-
-# In[2]:
-
-
-from fastapi import FastAPI, File, UploadFile, Response
-from fastapi.responses import StreamingResponse, FileResponse
+from fastapi import FastAPI, File, Query, UploadFile, HTTPException, Form
+from fastapi.responses import FileResponse, PlainTextResponse
 import uvicorn
 import joblib
 import numpy as np
 from pydantic import BaseModel
-import io
-import pandas as pd
-from io import BytesIO, StringIO
+
 
 
 app = FastAPI(
     title="Credit Card Fraud Detection API",
-    description="""An API that utilises a Machine Learning model that detects if a credit card transaction is fraudulent or not based on the following features: age, gender, blood pressure, smoke, coughing, allergies, fatigue etc.""",
-    version="0.1.0", debug=True)
+    description="""An API that utilises a Machine Learning model that detects if a credit card transaction is fraudulent or not based on the following features: hours, amount, transaction type etc.""",
+    version="1.0.0", debug=True)
 
 
-model = joblib.load('frauddetection.pkl')
+model = joblib.load('credit_fraud.pkl')
 
-@app.get('/')
-def home():
-    return {'Title': 'Credit Card Fraud Detection API'}
+@app.get("/", response_class=PlainTextResponse)
+async def running():
+  note = """
+Credit Card Fraud Detection API 🙌🏻
+
+Note: add "/docs" to the URL to get the Swagger UI Docs or "/redoc"
+  """
+  return note
+
+favicon_path = 'favicon.png'
+@app.get('/favicon.png', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
 																	
 class fraudDetection(BaseModel):
     step:int
@@ -43,31 +44,13 @@ class fraudDetection(BaseModel):
 def predict(data : fraudDetection):
                                                                                                                                                                                                                                 
     features = np.array([[data.step, data.types, data.amount, data.oldbalanceorig, data.newbalanceorig, data.oldbalancedest, data.newbalancedest, data.isflaggedfraud]])
-    model = joblib.load('frauddetection.pkl')
+    model = joblib.load('credit_fraud.pkl')
 
     predictions = model.predict(features)
     if predictions == 1:
         return {"fraudulent"}
     elif predictions == 0:
         return {"not fraudulent"}
-
-@app.post("/upload")
-async def upload(file: UploadFile = File(...)):
-    model = joblib.load('model.pkl')
-    contents = await file.file.read()
-    buffer = BytesIO(contents)
-    df = pd.read_csv(StringIO(str(buffer.file.read(), 'utf-16')), encoding='utf-16')
-    #df1 = df.to_numpy()
-    #predictions = model.predict(df1)
-    #predictions_list = pd.DataFrame(predictions, columns = ["Predictions"])
-    buffer.close()
-    #return predictions_list
-    
-    
-    return df.to_dict(orient='records')
-
-
-# In[ ]:
 
 
 
